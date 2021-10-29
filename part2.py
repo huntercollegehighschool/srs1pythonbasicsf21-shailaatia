354d4aa2f23a661a52abb3a792fd203e4acd73a8
shailaatia <75748878+shailaatia@users.noreply.github.com>
2021-10-29 16:31:54 +0000

    part 1 and 2

--- part2.py
+++ part2.py
@@ -1,6 +1,10 @@
+2
 """
 Define a function twodigitodd that take a single integer argument (number). The function should return True if the number is a two digit odd number, and False if it is not.
 """
 
 def twodigitodd(number):
-  pass  # delete this when you start writing your code
+  if number >= 10 and number > 99 and number/2 % 1 == 0:
+    return False
+  else:
+   return True
